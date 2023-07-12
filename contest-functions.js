d1be32e8cad9135fd4604b3ca01f476fdfdf1399
Ricardo Schuster <39931121+SCHSTR@users.noreply.github.com>
2023-07-12 00:28:31 -0300

    Updating variables

--- contest-functions.js
+++ contest-functions.js
@@ -1,9 +1,11 @@
-export function updateUserUI(userData){
-    console.log(userData)
-  loggedNav.style.display = "flex";
-  loginBanner.style.display = "none";
+export function updateUserUI(userData) {
+    let loggedNav = document.getElementById("votingAs");
+    let loginBanner = document.getElementById("login_banner");
 
-  loggedNav.innerHTML = `
+    loggedNav.style.display = "flex";
+    loginBanner.style.display = "none";
+
+    loggedNav.innerHTML = `
     <div class="x">
         <div style="text-align: right">
         <p class="voting_as">Voting as:</p>
@@ -13,49 +15,49 @@ export function updateUserUI(userData){
         <img src="${userData.photoURL}" style="width: 36px; height: 36px; border-radius: 100%">
     </div>
 `
-  const logoutButton = document.getElementById("logout-button");
-  logoutButton.addEventListener("click", function () {
-    signOut(auth).then(() => {
-      window.location.reload()
-    }).catch((error) => { });
-  })
+    const logoutButton = document.getElementById("logout-button");
+    logoutButton.addEventListener("click", function () {
+        signOut(auth).then(() => {
+            window.location.reload()
+        }).catch((error) => { });
+    })
 }
 
-export function callVote(btn, userInfo, origin) {
-  console.log("Button clicked")
-  let user = userInfo;
-  let slug = btn.getAttribute('slug');
-  let totalVotes = btn.childNodes[1].innerText;
+export function callVote(btn, userInfo, origin, url) {
+    console.log("Button clicked")
+    let user = userInfo;
+    let slug = btn.getAttribute('slug');
+    let totalVotes = btn.childNodes[1].innerText;
 
-  fetch(currentEnvironment + `/new-vote/${slug}&${user}&${contestName}&${origin}`, {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${userToken}`
-    },
-    body: JSON.stringify({ success: true })
-  })
-    .then(response => {
-    return response.json()
-  })
-    .then(json => {
-    if (json.vote) {
-      let vote = parseInt(totalVotes) + 1
-      btn.childNodes[1].innerText = vote.toString()
-      btn.classList.add("success")
-    }
-  })
-    .catch(err => { console.log(err) })
+    fetch(url + `/new-vote/${slug}&${user}&${contestName}&${origin}`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${userToken}`
+        },
+        body: JSON.stringify({ success: true })
+    })
+        .then(response => {
+            return response.json()
+        })
+        .then(json => {
+            if (json.vote) {
+                let vote = parseInt(totalVotes) + 1
+                btn.childNodes[1].innerText = vote.toString()
+                btn.classList.add("success")
+            }
+        })
+        .catch(err => { console.log(err) })
 }
 
 export function getUserToken() {
-  const localStorageToken = localStorage.getItem('access_token');
-  if (localStorageToken) {return localStorageToken;}
-  const cookieToken = document.cookie
-  .split('; ')
-  .find(row => row.startsWith('access_token='));
-  if (cookieToken) {
-    return cookieToken.split('=')[1];
-  }
-  return null;
+    const localStorageToken = localStorage.getItem('access_token');
+    if (localStorageToken) { return localStorageToken; }
+    const cookieToken = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('access_token='));
+    if (cookieToken) {
+        return cookieToken.split('=')[1];
+    }
+    return null;
 }
