8f06e7a27c2ceda40feb729c42bde8295996be58
Ricardo Schuster <39931121+SCHSTR@users.noreply.github.com>
2023-07-13 14:49:56 -0300

    Fix and Add functions
    Fixed function to Update user UI and added a global function to call error handler in all pages

--- contest-functions.js
+++ contest-functions.js
@@ -1,7 +1,7 @@
 import { signOut } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 
 
-export function updateUserUI(userData, userAuth) {
+function updateUserUI(userData, userAuth) {
     let loggedNav = document.getElementById("votingAs");
     let loginBanner = document.getElementById("login_banner");
 
@@ -9,22 +9,41 @@ export function updateUserUI(userData, userAuth) {
     loginBanner.style.display = "none";
 
     loggedNav.innerHTML = `
-    <div class="x">
-        <div style="text-align: right">
-        <p class="voting_as">Voting as:</p>
-        <p style="margin-bottom: 0; font-size: 0.8rem">${userData.displayName}</p>
-        <p id="logout-button">Logout</p>
-    </div>
-        <img src="${userData.photoURL}" style="width: 36px; height: 36px; border-radius: 100%">
-    </div>
-`
-    const logoutButton = document.getElementById("logout-button");
-    logoutButton.addEventListener("click", function () {
-        signOut(userAuth).then(() => {
-            window.location.reload()
-        }).catch((error) => { });
-    })
-}
+      <div class="x">
+          <div style="text-align: right; display: flex; justify-content: center; flex-direction: column;">
+          <p style="margin-bottom: 0; font-size: 0.8rem; color: white">${userData.displayName}</p>
+          <p id="logout-button" style="margin-bottom: 0px">Logout</p>
+      </div>
+          <div style="position: relative">
+              ${renderLoginMethod(userData.providerData[0].providerId)}
+              <img src="${userData.photoURL}" style="width: 36px; height: 36px; border-radius: 100%">
+          </div>
+      </div>
+  `
+      const logoutButton = document.getElementById("logout-button");
+      logoutButton.addEventListener("click", function () {
+          signOut(userAuth).then(() => {
+              window.location.reload()
+          }).catch((error) => { });
+      })
+  }
+
+  function renderLoginMethod(providerId){
+      let html;
+      switch (providerId) {
+          case "google.com":
+              html = `
+                  <img src="https://www.transparentpng.com/thumb/google-logo/google-logo-png-icon-free-download-SUF63j.png" style="position: absolute; right: 0; bottom: 0; width: 16px; height: 16px" />
+              `
+              break;
+          case "twitter.com":
+              html =`
+                  <img src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png" style="position: absolute; right: 0; bottom: 0; width: 16px; height: 16px" />
+              `
+              break;
+      }
+    return html
+  }
 
 export function callVote(btn, userInfo, origin, url) {
     console.log("Button clicked")
@@ -63,4 +82,25 @@ export function getUserToken() {
         return cookieToken.split('=')[1];
     }
     return null;
+}
+
+export function callError (title, message){
+    let toast = document.getElementById("toastMessage");
+    let toastHeading = document.getElementById("toastHeading");
+    let toastText = document.getElementById("toastText");
+
+    toastHeading.innerText = title || "No description provided"
+    toastText.innerText = message || "No title provided"
+
+    toast.classList.add("error")
+    toast.style.opacity = "1";
+    toast.style.marginBottom = "132px"
+    toast.style.transition = "all 650ms"
+
+    setTimeout(() => {
+        toast.style.opacity = "0";
+        toast.style.marginBottom = "-100px"
+        toast.style.transition = "all 650ms"
+        toast.classList.remove("error")
+    }, 5000);
 }
